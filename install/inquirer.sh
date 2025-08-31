c2ace128d3980abe1050bd5e55e4c83b158f7551
Stumie <Stumie123@googlemail.com>
2025-08-31 20:36:11 +0200

    i don't want to understand this script. it shall just work!

--- install/inquirer.sh
+++ install/inquirer.sh
@@ -5,67 +5,65 @@
 # SPDX-License-Identifier: MIT
 # For original source, see https://github.com/kahkhang/Inquirer.sh
 
-# Replaced original functions with 'dialog' by a 'kdialog' variant envisioned from ChatGPT
+# Replaced original functions with 'dialog' by a 'kdialog' variant envisioned from ChatGPT and Google Gemini
 
 ### GLOBAL CONSTANTS ###
-declare -r ANSI_LIGHT_BLUE="\033[1;94m"
-declare -r ANSI_LIGHT_GREEN="\033[92m"
-declare -r ANSI_CLEAR_TEXT="\033[0m"
+declare -r ANSI_LIGHT_BLUE="\033[1;94m" # Light blue text.
+declare -r ANSI_LIGHT_GREEN="\033[92m"  # Light green text.
+declare -r ANSI_CLEAR_TEXT="\033[0m"    # Default text.
 
 ### FUNCTIONS ###
-function inqMenu() {
-    declare DIALOG_TEXT="$1"
-    declare INPUT_OPTIONS_VAR="$2"
-    declare RETURN_STRING_VAR="$3"
-    declare -n INPUT_OPTIONS="$INPUT_OPTIONS_VAR"
-    declare -n RETURN_STRING="$RETURN_STRING_VAR"
 
-    # Menüeinträge vorbereiten (key1 label1 key2 label2 ...)
-    declare DIALOG_OPTIONS=()
+function inqMenu() {
+    # Named references for passing variables
+    declare -n INPUT_OPTIONS_REF="$2"
+    declare -n RETURN_STRING_REF="$3"
+    declare DIALOG_TEXT="$1"
+
+    # Prepare menu options (key1 label1 key2 label2 ...)
+    local DIALOG_OPTIONS=()
     local i=1
-    for OPTION in "${INPUT_OPTIONS[@]}"; do
+    for OPTION in "${INPUT_OPTIONS_REF[@]}"; do
         local TRIMMED="$(echo "$OPTION" | sed 's/^[ \t]*//;s/[ \t]*$//')"
         DIALOG_OPTIONS+=("$i" "$TRIMMED")
         ((i++))
     done
 
-    # Menü anzeigen
+    # Show menu
     local SELECTED_KEY
-    SELECTED_KEY=$(kdialog --menu "$DIALOG_TEXT" "${DIALOG_OPTIONS[@]}") || exit 0
+    # 2>&1 >/dev/tty redirects kdialog's output to be correctly captured in SELECTED_KEY
+    SELECTED_KEY=$(kdialog --menu "$DIALOG_TEXT" "${DIALOG_OPTIONS[@]}" 2>&1 >/dev/tty)
+    
+    # Check if the dialog was cancelled (e.g., by pressing "Cancel")
+    if [[ $? -ne 0 ]]; then
+        echo "Dialog was cancelled." >&2
+        return 1
+    fi
 
-    # Rückgabe-Index in tatsächliche Option umwandeln
-    RETURN_STRING="${INPUT_OPTIONS[$((SELECTED_KEY - 1))]}"
+    # Convert the selected index (1-based) to the actual option
+    RETURN_STRING_REF="${INPUT_OPTIONS_REF[$((SELECTED_KEY - 1))]}"
 
-    # Ausgabe
-    echo -e "${ANSI_LIGHT_GREEN}Q) ${ANSI_CLEAR_TEXT}${ANSI_LIGHT_BLUE}${DIALOG_TEXT}${ANSI_CLEAR_TEXT} --> ${ANSI_LIGHT_GREEN}${RETURN_STRING}${ANSI_CLEAR_TEXT}"
+    # Output
+    echo -e "${ANSI_LIGHT_GREEN}Q) ${ANSI_CLEAR_TEXT}${ANSI_LIGHT_BLUE}${DIALOG_TEXT}${ANSI_CLEAR_TEXT} --> ${ANSI_LIGHT_GREEN}${RETURN_STRING_REF}${ANSI_CLEAR_TEXT}"
+    return 0
 }
 
 function inqChkBx() {
+    # Named references for passing variables
+    declare -n INPUT_OPTIONS_REF="$2"
+    declare -n RETURN_ARRAY_REF="$3"
     declare DIALOG_TEXT="$1"
-    declare INPUT_OPTIONS_VAR="$2"
-    declare RETURN_ARRAY_VAR="$3"
-    declare -n INPUT_OPTIONS="$INPUT_OPTIONS_VAR"
-    declare -n RETURN_ARRAY="$RETURN_ARRAY_VAR"
 
-    # Checkliste vorbereiten
-    declare DIALOG_OPTIONS=()
+    # Prepare checklist options (key1 label1 off key2 label2 off ...)
+    local DIALOG_OPTIONS=()
     local i=1
-    for OPTION in "${INPUT_OPTIONS[@]}"; do
+    for OPTION in "${INPUT_OPTIONS_REF[@]}"; do
         local TRIMMED="$(echo "$OPTION" | sed 's/^[ \t]*//;s/[ \t]*$//')"
         DIALOG_OPTIONS+=("$i" "$TRIMMED" "off")
         ((i++))
     done
 
-    # Checkliste anzeigen
+    # Show checklist
     local SELECTED_KEYS
-    SELECTED_KEYS=$(kdialog --checklist "$DIALOG_TEXT" "${DIALOG_OPTIONS[@]}") || exit 0
-
-    # Rückgabe in tatsächliche Optionen umwandeln
-    IFS=" " read -r -a KEYS <<< "$SELECTED_KEYS"
-    for KEY in "${KEYS[@]}"; do
-        RETURN_ARRAY+=("${INPUT_OPTIONS[$((KEY - 1))]}")
-    done
-
-    # Ausgabe
-    echo -e "${ANSI_LIGHT_GREEN}Q) ${ANSI_CLEAR_TEXT}${ANSI_LIGHT_BLUE}${DIALOG_TEXT}${ANSI_CLEAR_TEXT} --> ${ANSI_LIGHT_GREEN}${RETURN_ARRAY[*]}${ANSI_CLEAR_TEXT}"
-}
+    # 2>&1 >/dev/tty redirects kdialog's output to be correctly captured in SELECTED_KEYS
+    SELECTED_KEYS=$(kdialog --checklist "$DIALOG_TEXT" "${DIALOG_OPTIONS[@]}" 2>&1 >/dev/
